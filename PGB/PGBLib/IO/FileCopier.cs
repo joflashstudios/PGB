a1abb3d0e70f580db664084dec034637069c0601
Josiah Roberts <joflashstudios@gmail.com>
2015-11-18 09:53:33 +0100

    More FileCopier work. Should be usable now.

--- PGB/PGBLib/IO/FileCopier.cs
+++ PGB/PGBLib/IO/FileCopier.cs
@@ -17,50 +17,65 @@ namespace PGBLib.IO
         // Enums
         // These Enums are used for the windows CopyFileEx function
         [Flags]
-        private enum CopyFileFlags : uint
+        internal enum CopyFileFlags : uint
         {
             COPY_FILE_FAIL_IF_EXISTS = 0x00000001,
             COPY_FILE_RESTARTABLE = 0x00000002,
             COPY_FILE_OPEN_SOURCE_FOR_WRITE = 0x00000004,
             COPY_FILE_ALLOW_DECRYPTED_DESTINATION = 0x00000008
         }
-        private enum CopyProgressResult : uint
+        internal enum CopyProgressResult : uint
         {
             PROGRESS_CONTINUE = 0,
             PROGRESS_CANCEL = 1,
             PROGRESS_STOP = 2,
             PROGRESS_QUIET = 3
         }
-        private enum CopyProgressCallbackReason : uint
+        internal enum CopyProgressCallbackReason : uint
         {
             CALLBACK_CHUNK_FINISHED = 0x00000000,
             CALLBACK_STREAM_SWITCH = 0x00000001
         }
       
         // Delegates
-        private delegate CopyProgressResult CopyProgressRoutine(Int64 TotalFileSize, Int64 TotalBytesTransferred, Int64 StreamSize, Int64 StreamBytesTransferred, UInt32 dwStreamNumber, CopyProgressCallbackReason dwCallbackReason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData);
-        private delegate CopyProgressResult DEL_CopyProgressHandler(Int64 total, Int64 transferred, Int64 streamSize, Int64 StreamByteTrans, UInt32 dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData);
+        internal delegate CopyProgressResult CopyProgressRoutine(Int64 TotalFileSize, Int64 TotalBytesTransferred, Int64 StreamSize, Int64 StreamBytesTransferred, UInt32 dwStreamNumber, CopyProgressCallbackReason dwCallbackReason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData);
 
         // Kernal32 Calls
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         [return: MarshalAs(UnmanagedType.Bool)]
         static extern unsafe bool CopyFileEx(string lpExistingFileName, string lpNewFileName, CopyProgressRoutine lpProgressRoutine, IntPtr lpData, Boolean* pbCancel, CopyFileFlags dwCopyFlags);
 
-        private CopyProgressResult CopyProgressHandler(Int64 total, Int64 transferred, Int64 streamSize, Int64 StreamByteTrans, UInt32 dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData)
+        private static CopyProgressResult CopyProgressHandler(Int64 total, Int64 transferred, Int64 streamSize, Int64 StreamByteTrans, UInt32 dwStreamNumber, CopyProgressCallbackReason reason, IntPtr hSourceFile, IntPtr hDestinationFile, IntPtr lpData)
         {            
             //TODO: progress logic goes here
             return CopyProgressResult.PROGRESS_CONTINUE;
         }
 
-        internal void Copy(string source, string destination, ref bool cancel)
+        internal static void Copy(string source, string destination)
+        {
+            bool dummycancel = false;
+            Copy(source, destination, ref dummycancel);
+        }
+
+        internal static void Copy(string source, string destination, ref bool cancel)
+        {
+            Copy(source, destination, ref cancel, new CopyProgressRoutine(CopyProgressHandler));
+        }
+
+        internal static void Copy(string source, string destination, ref bool cancel, CopyProgressRoutine progressHandler)
+        {
+            Copy(source, destination, ref cancel, progressHandler, 0);
+        }
+
+        internal static void Copy(string source, string destination, ref bool cancel, CopyProgressRoutine progressHandler, CopyFileFlags flags)
         {
             unsafe
             {
                 fixed (Boolean* cancelp = &cancel)
                 {
-                    CopyFileEx(source, destination, new CopyProgressRoutine(this.CopyProgressHandler), IntPtr.Zero, cancelp, 0);
+                    CopyFileEx(source, destination, progressHandler, IntPtr.Zero, cancelp, flags);
                 }
             }
-        }
+        }        
     }
 }
