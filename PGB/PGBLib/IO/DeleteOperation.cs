00bc81fd9c98705b0dc0ef30a9ba303f47c81af5
Josiah Roberts <joflashstudios@gmail.com>
2015-11-19 22:51:53 +0100

    Switched up some files

---
+++ PGB/PGBLib/IO/DeleteOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGBLib.IO
+{
+    class DeleteOperation : IOOperation
+    {
+        /// <summary>
+        /// Whether to delete the folder after deleting the last file out of it on a delete operation
+        /// </summary>
+        public bool DeleteEmptyFolder { get; set; }
+
+        public override void DoOperation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PGB/Tester/IOTest.cs
+++ PGB/Tester/IOTest.cs
@@ -10,11 +10,17 @@ namespace PGBLib.IO.Win32
     {
         public static void RunTest()
         {
-            CopyOperation op = new CopyOperation();
-            op.File = @"C:\Users\Elizabeth\Documents\Niger History\Maradi Region\Atchire\Book worthy\IMG_1631.JPG";
-            op.CreateFolder = true;
-            op.TransferDestination = @"C:\Users\Elizabeth\Documentssss\ssssssomething\Niger History\Maradi Region\Atchire\Book worthy\IMG_1631.JPG";
-            op.DoOperation();
+            StringBuilder numbers = new StringBuilder();
+            Random r = new Random();
+            for (int x = 1; x < 20; x++)
+            {
+                numbers.Append(r.Next(1, 12589) + "\n");
+                System.Threading.Thread.Sleep(75);
+                r = new Random(r.Next(900));                
+            }
+            Console.Write(numbers);
+            System.IO.File.WriteAllText("C:\\users\\elizabeth\\numbers.txt", numbers.ToString());
+            Console.ReadLine();
         }
     }
 }
