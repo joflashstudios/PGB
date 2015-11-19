dba40fed24c88c1c8d96e2d772aaab15442c137d
Josiah Roberts <joflashstudios@gmail.com>
2015-11-19 14:33:29 +0100

    Working on IOOperation and OperationWorker systems. One more layer up from FileCopier and FolderCloner.

--- PGB/PGBLib/IO/IOOperation.cs
+++ PGB/PGBLib/IO/IOOperation.cs
@@ -6,43 +6,13 @@ using System.Threading.Tasks;
 
 namespace PGBLib.IO
 {
-    class IOOperation
+    abstract class IOOperation
     {
-        /// <summary>
-        /// The type of file operation
-        /// </summary>
-        public IOOperationType Type { get; set; }
-
-        /// <summary>
-        /// Whether to overwrite or throw an exception if the file already exists
-        /// </summary>
-        public bool Overwrite { get; set; }
-
-        /// <summary>
-        /// Whether to create the destination folder or throw an exception if the folder does not exist
-        /// </summary>        
-        public bool CreateFolder { get; set; }
-
-        /// <summary>
-        /// Whether to delete the folder after deleting the last file out of it on a delete operation
-        /// </summary>
-        public bool DeleteEmptyFolder { get; set; }
-
         /// <summary>
         /// The file to operate on
         /// </summary>
         public string File { get; set; }
 
-        /// <summary>
-        /// The destination (if any) to move/copy the file to
-        /// </summary>
-        public string TransferDestination { get; set; }
-    }
-
-    public enum IOOperationType
-    {
-        Copy,
-        Move,
-        Delete
+        public abstract void DoOperation();
     }
 }
--- PGB/PGBLib/IO/OperationWorker.cs
+++ PGB/PGBLib/IO/OperationWorker.cs
@@ -2,12 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace PGBLib.IO
 {
-    internal class OperationWorker
+    internal class OperationWorker : IDisposable
     {
+        public OperationWorker()
+        {
+            _workerThread = new Thread(new ThreadStart(DoWork));
+        }
 
+        public Queue<IOOperation> OperationQueue { get; set; }
+
+        private bool _terminate = false;
+        private Thread _workerThread;
+        public Thread WorkerThread { get { return _workerThread; } }
+
+        public void Start()
+        {
+            _workerThread.Start();
+        }
+
+        private void DoWork()
+        {
+            while (!_terminate)
+            {
+                IOOperation currentOperation = null;
+                lock (OperationQueue)
+                {
+                    if (OperationQueue.Count > 0)
+                        currentOperation = OperationQueue.Dequeue();
+                }
+
+                if (currentOperation != null)
+                {
+                    
+                }
+                else //We have nothing to do. Yield our current time slice.
+                {
+                    Thread.Sleep(0);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _terminate = true;
+        }
+
+        public void Terminate()
+        {
+            Dispose();
+        }
+        //public List<IOOperation> CompletedOperations { get; set; } //What's the best way to get this information to the OperationManager? Maybe actually use a WorkerProgress event.... yeah.
     }
 }
