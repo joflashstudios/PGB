66c957d0a27ebb18799256beecb3aa58dda12f3a
Josiah Roberts <joflashstudios@gmail.com>
2015-11-21 21:25:02 +0100

    Added OperationProgressDetails

---
+++ PGB/PGBLib/IO/OperationProgressDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGBLib.IO
+{
+    class OperationProgressDetails
+    {        
+        IOOperation Operation { get; }
+        bool Completed { get; }
+        long BytesTransferred { get; }
+        long BytesTotal { get; }
+        Exception Error { get; }
+
+        public OperationProgressDetails(IOOperation operation, bool completed)
+        {
+            Operation = operation;
+            Completed = completed;
+        }
+
+        public OperationProgressDetails(IOOperation operation, Exception error)
+        {
+            Operation = operation;
+            this.Error = error;
+        }
+
+        public OperationProgressDetails(IOOperation operation, long bytesTransferred, long bytesTotal)
+        {
+            Operation = operation;
+            BytesTransferred = bytesTransferred;
+            BytesTotal = bytesTotal;
+        }
+    }
+}
